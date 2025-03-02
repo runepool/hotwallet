a562f1d1201958269654bbb38dac0f48049d7f21
0xVersion <dev@0xVersion.com>
2025-03-02 15:06:00 +0200

    Set swagger password

--- apps/exchange/src/main.ts
+++ apps/exchange/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ExchangeModule } from './exchange.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import expressBasicAuth from 'express-basic-auth';
 
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
@@ -9,15 +10,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ExchangeModule);
-  
+
   app.enableCors();
+  // Sometime after NestFactory add this to add HTTP Basic Auth
+  app.use(
+    ['/docs', '/docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.API_MANAGER || 'admin']: process.env.API_PASSWORD || 'password',
+      },
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Exchange API')
     .setDescription('The Exchange API description')
     .setVersion('1.0')
     .addTag('exchange')
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
