5ba8f42c754b60004df7886ff6ba6931a7995c93
0xVersion <dev@0xVersion.com>
2025-04-06 19:35:45 +0300

    Fix argon2 import to use require instead of ES6 import syntax

--- libs/wallet/src/encryption.service.ts
+++ libs/wallet/src/encryption.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as crypto from 'crypto';
-import * as argon2 from 'argon2';
+const argon2 = require('argon2');
 
 @Injectable()
 export class EncryptionService {
