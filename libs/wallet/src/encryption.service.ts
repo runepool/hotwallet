1d1c0c8a36b5a1e515de763d4f937dc943feabf1
0xVersion <dev@0xVersion.com>
2025-04-06 19:25:28 +0300

    Fix argon2 import syntax to use CommonJS require style

--- libs/wallet/src/encryption.service.ts
+++ libs/wallet/src/encryption.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as crypto from 'crypto';
-import argon2 from 'argon2';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class EncryptionService {
